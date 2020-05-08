0a55eef301955700e3f58e81e154512a388f1658
jijo0465 <jijo0465@gmail.com>
2020-05-08 16:17:51 +0530

    After Delete

--- lib/screens/classroom_screen.dart
+++ lib/screens/classroom_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:parent_app/models/grade.dart';
 import 'package:parent_app/models/student.dart';
+import 'package:parent_app/states/student_state.dart';
+import 'package:provider/provider.dart';
 
 class ClassroomScreen extends StatefulWidget {
   // final Grade grade = Grade();
@@ -14,23 +16,36 @@ class ClassroomScreen extends StatefulWidget {
 }
 
 class _ClassroomScreenState extends State<ClassroomScreen> {
-  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
-  var _key;
+  // final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+  // var _key;
   // ListModel<int> _list;
+  Student _selectedStudent;
+  List<Widget> discussionListWidget = [];
   List<DocumentSnapshot> _items;
+  DocumentSnapshot addItem;
   Grade grade = Grade.empty();
-  int id = 3001;
+  int id = 4001;
+  int widgetIndex;
 
   @override
   void initState() {
     // TODO: implement initState
+    widgetIndex = 0;
     grade.setId(id);
-
     super.initState();
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    discussionListWidget.clear();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    StudentState state = Provider.of<StudentState>(context, listen: true);
+    _selectedStudent = state.selectedstudent;
     return Scaffold(
         body: Column(children: <Widget>[
       Container(
@@ -40,14 +55,43 @@ class _ClassroomScreenState extends State<ClassroomScreen> {
       ),
       SizedBox(height: 12),
       Container(
+        height: 40,
+        width: MediaQuery.of(context).size.width - 40,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20), color: Colors.blue),
+        alignment: Alignment.center,
         child: Text(
           'Discussions',
-          textAlign: TextAlign.left,
+          textAlign: TextAlign.center,
           style: TextStyle(
-            color: Colors.blue,
+            color: Colors.white,
             fontSize: 16,
           ),
         ),
+      ),
+      SizedBox(height: 12,),
+      Container(
+        height: 40,
+        width: MediaQuery.of(context).size.width - 40,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20)),
+        child: TextField(
+          textAlignVertical: TextAlignVertical.center,
+          textAlign: TextAlign.start,
+          cursorColor: Colors.blue,
+          decoration: InputDecoration(
+            hintText: 'add to discussions...',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),  
+          ),
+          autofocus: true,
+          onSubmitted: (text){
+            print('submit');
+            _addToDiscussions(text);
+            text = '';
+          },
+        ),      
       ),
       StreamBuilder<QuerySnapshot>(
           // key: _key,
@@ -65,109 +109,81 @@ class _ClassroomScreenState extends State<ClassroomScreen> {
               );
             else {
               _items = snapshot.data.documents;
-              print(_items.length);
+              listItem(_items);
+              print('item: ${_items[0]}');
               // setState(() {
               // AnimatedList.of(context).insertItem(0);
-              Future.delayed(Duration(milliseconds: 200))
-                  .then((value) => _listKey.currentState.insertItem(0));
+              // Future.delayed(Duration(milliseconds: 200))
+              //     .then((value) => _listKey.currentState.insertItem(0));
 
               // });
-              return Container();
+              // return listItem(_items[0]);
+              return Expanded(
+                  child: SingleChildScrollView(
+                      child: Column(children: discussionListWidget.reversed.toList())
+                      // child: listItem(_items[0]['disussion'])
+                      ));
             }
           }),
-      // AnimatedList.of(context).insertItem(_items.length+1);
-      AnimatedList(
-        key: _listKey,
-        initialItemCount: 0,
-        // reverse: true,
-        shrinkWrap: true,
-        itemBuilder: (context, index, animation) {
-          // var documentReference = snapshot;
-          // var document = snapshot.data.documents.map(DocumentSnapshot document);
-          // return ListTile(
-          //   title: Text(document['title']),
-          //   subtitle: Text(document['author']),
-          // );
-          // var discussion = documentReference[index];
-          // _items = documentReference[index];
-          return listItem(_items[index]);
-          // Column(children: <Widget>[
-          //   Container(
-          //     height: 50,
-          //     width: MediaQuery.of(context).size.width * 5 / 6,
-          //     child: Row(
-          //       children: <Widget>[
-          //         Container(
-          //           height: 40,
-          //           width: 40,
-          //           // margin: EdgeInsets.only(left: 20),
-          //           decoration: BoxDecoration(
-          //             shape: BoxShape.circle,
-          //             image: DecorationImage(
-          //                 image: NetworkImage(
-          //                     'https://www.sean-christopher.com/seanchristopher/wp-content/uploads/2014/10/hotforteacher.png'),
-          //                 fit: BoxFit.fill),
-          //           ),
-          //         ),
-          //         Expanded(
-          //             child: Container(
-          //           padding: EdgeInsets.only(left: 20, right: 20),
-          //           height: 50,
-          //           width: MediaQuery.of(context).size.width,
-          //           child: Align(
-          //             alignment: Alignment.centerLeft,
-          //             child: Text(discussion['discussion']),
-          //           ),
-          //         )),
-          //       ],
-          //     ),
-          //   ),
-          //   Divider(
-          //     color: Colors.black,
-          //     thickness: 2,
-          //   )
-          // ]);
-        },
-      ),
     ]));
   }
 
-  Widget listItem(DocumentSnapshot item) {
-    return Column(children: <Widget>[
-      Container(
-        height: 50,
-        width: MediaQuery.of(context).size.width * 5 / 6,
-        child: Row(
-          children: <Widget>[
-            Container(
-              height: 40,
-              width: 40,
-              // margin: EdgeInsets.only(left: 20),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                    image: NetworkImage(
-                        'https://www.sean-christopher.com/seanchristopher/wp-content/uploads/2014/10/hotforteacher.png'),
-                    fit: BoxFit.fill),
+  listItem(List<DocumentSnapshot> item) {
+    for (; widgetIndex < item[0]['disussion'].length; widgetIndex++) {
+      print('itemval: ${item[0]['disussion'][widgetIndex]['comment']}');
+      discussionListWidget.add(Column(children: <Widget>[
+        Container(
+          height: 50,
+          width: MediaQuery.of(context).size.width * 5 / 6,
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 40,
+                width: 40,
+                // margin: EdgeInsets.only(left: 20),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: 
+                      // AssetImage(''),
+                        NetworkImage(
+                          'https://www.sean-christopher.com/seanchristopher/wp-content/uploads/2014/10/hotforteacher.png'),
+                      fit: BoxFit.fill),
+                ),
               ),
-            ),
-            Expanded(
-                child: Container(
-              padding: EdgeInsets.only(left: 20, right: 20),
-              height: 50,
-              width: MediaQuery.of(context).size.width,
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(item['discussion']),
-              ),
-            )),
-          ],
+              Expanded(
+                  child: Container(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(item[0]['disussion'][widgetIndex]['comment']),
+                ),
+              )),
+            ],
+          ),
         ),
-      ),
-      Divider(
-        color: Colors.black,
-        thickness: 2,
-      )
-    ]);
+        Divider(
+          color: Colors.black,
+          thickness: 2,
+        )
+      ]));
+    }
+  }
+
+  _addToDiscussions(String text)
+  {
+    widgetIndex++;
+    Firestore.instance.runTransaction((transaction) async {
+    await transaction.update(Firestore.instance.collection('classroom_${grade.id}').document('session_1'), {
+        'disussion' : {
+          [widgetIndex] : {
+            'comment': text,
+            'date': DateTime.now().toUtc(),
+          }
+        }
+      });
+    });
   }
 }
