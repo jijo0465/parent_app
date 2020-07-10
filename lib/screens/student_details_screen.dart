d26a7fb054a1a9525cd4338a509ca8a946df2716
jijo0465 <jijo0465@gmail.com>
2020-07-10 12:47:00 +0530

    Demo First Release iOS

--- lib/screens/student_details_screen.dart
+++ lib/screens/student_details_screen.dart
@@ -1,19 +1,15 @@
 import 'dart:ui';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:parent_app/components/digicampus_appbar.dart';
 import 'package:parent_app/components/digi_key_value_display.dart';
 import 'package:parent_app/models/student.dart';
-import 'package:parent_app/services/digi_attendance.dart';
 import 'package:parent_app/states/login_state.dart';
 import 'package:parent_app/states/student_state.dart';
 import 'package:provider/provider.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart';
 import 'dart:math';
 import 'dart:io';
 
